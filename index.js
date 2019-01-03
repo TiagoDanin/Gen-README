fdfbeaf5e47850b0f546cb807f372b41272982bd
TiagoDanin <TiagoDanin@outlook.com>
2019-01-03 11:10:28 -0300

    feat: preparation use of api and scan more examples

--- index.js
+++ index.js
@@ -1,91 +1,78 @@
 #!/usr/bin/env node
-'use strict'
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+const mustache = require('mustache')
+const { prompt } = require('enquirer')
+const gh = require('github-url-to-object')
 
-var hogan = require('hogan.js')
-var fs = require('fs')
-var path = require('path')
-var util = require('util')
-var argv = require('yargs')
-  .usage('Usage: readme path/to/package.json')
-  .check(function (argv) {
-    if (!argv._.length) throw new Error('A path to a valid package.json is required')
-    return true
-  })
-  .option('r', {
-    alias: 'travis',
-    description: 'display a travis badge'
-  })
-  .option('t', {
-    alias: 'test',
-    description: 'include test output in readme'
-  })
-  .alias('t', 'tests')
-  .help('help')
-  .alias('h', 'help')
-  .argv
-var gh = require('github-url-to-object')
-var execSync = require('sync-exec')
-var stripAnsi = require('strip-ansi')
-
-var pkgPath = path.resolve(process.cwd(), argv._[0])
-
-try {
-  var pkg = require(pkgPath)
-} catch (e) {
-  console.error('Invalid JSON file: %s', pkgPath)
-  process.exit()
+const getInfoDeps = (deps) => {
+	return Object.keys(deps).map((dep) => {
+		return {
+			name: dep,
+			repository: `https://ghub.io/${{dep}}`,
+			description: '' //TODO GET VIA API NPM
+		}
+	})
 }
 
-pkg.private = pkg.private || pkg.license === 'private' || false
+const main = async() => {
+	var pkg = {
+		name: '',
+		description: '',
+		scripts: {
+			test: false
+		},
+		repository: 'https://github.com/user/repo.git',
+		dependencies: {},
+		devDependencies: {},
+		preferGlobal: false
+	}
+	const opt = {
+		travis: true
+	}
+	const packageFile = path.resolve(`${process.cwd()}/package.json`)
+	pkg = _.merge(
+		pkg,
+		JSON.parse(fs.readFileSync(packageFile).toString())
+	)
+	pkg.gh = gh(pkg.repository.url || pkg.repository)
 
-if (argv.travis) {
-  if (pkg.repository && pkg.repository.url && gh(pkg.repository.url)) {
-    pkg.travis_url = gh(pkg.repository.url).travis_url
-  } else {
-    console.error('`repository.url` must be a GitHub repository URL for Travis to work')
-    process.exit()
-  }
+	const extensions = ['js', 'sh']
+	const files = ['example', '.env.example', 'usage', '.env.usage']
+	extensions.forEach((ext) => {
+		files.forEach((file) => {
+			let exampleFile = path.resolve(`${process.cwd()}/${file}.${ext}`)
+			if (fs.existsSync(exampleFile)) {
+				pkg.usage = {
+					language: ext,
+					content: fs.readFileSync(exampleFile).toString()
+				}
+
+				if (ext == 'js') {
+					//replace require('./')
+					pkg.usage.content = pkg.usage.content.replace(
+						/require\(['"]?\.\/['"]?\)/,
+						`require("${pkg.name}")`
+					)
+				}
+			}
+		})
+	})
+
+	pkg.dependencies = getInfoDeps(pkg.dependencies)
+	pkg.devDependencies = getInfoDeps(pkg.devDependencies)
+
+	//https://npmjs.org/package/{{name}}
+	//https://img.shields.io/npm/v/{{name}}.svg?style=flat-square
+	//https://img.shields.io/node/v/{{name}}.svg?style=flat-square
+	//https://img.shields.io/npm/dt/{{name}}.svg?style=flat-square
+	//https://img.shields.io/travis/{{gh.user}}/{{gh.repo}}.svg?branch=master&style=flat-square
+	//[![title](badge)](url)
+
+	const template = fs.readFileSync(path.join(__dirname, 'template.md')).toString()
+	const readme = mustache.render(template, pkg)
+	console.log(readme)
+	fs.writeFileSync('README.md', readme)
 }
-
-// Run tests and fetch output
-if (argv.tests || argv.test) {
-  pkg.testOutput = stripAnsi(execSync('npm test').stdout)
-    .replace(/\r/g, '') // remove weird newlines
-    .replace(/\n+/g, '\n') // remove excess newlines
-}
-
-// Look for example.js or example.sh in package.json directory
-var extensions = ['js', 'sh']
-extensions.forEach(function (language) {
-  var exampleFile = path.resolve(path.dirname(argv._[0])) + '/example.' + language
-  if (fs.existsSync(exampleFile)) {
-    pkg.usage = {
-      language: language,
-      content: fs.readFileSync(exampleFile).toString()
-    }
-
-    // replace require('./') statement with the package name
-    if (language === 'js') {
-      pkg.usage.content = pkg.usage.content.replace(
-        /require\(['"]?\.\/['"]?\)/,
-        util.format('require("%s")', pkg.name)
-      )
-    }
-  }
-})
-
-var getDeps = function (deps) {
-  return Object.keys(deps).map(function (depname) {
-    var dep = require(path.resolve(path.dirname(argv._[0])) + '/node_modules/' + depname + '/package.json')
-    dep.repository = 'https://ghub.io/' + depname
-    return dep
-  })
-}
-
-if (pkg.dependencies) pkg.depDetails = getDeps(pkg.dependencies)
-if (pkg.devDependencies) pkg.devDepDetails = getDeps(pkg.devDependencies)
-
-var templatePath = path.join(__dirname, 'template.md')
-var template = hogan.compile(fs.readFileSync(templatePath).toString())
-
-process.stdout.write(template.render(pkg))
+main()
